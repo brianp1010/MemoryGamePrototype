3b6ecbf71eb8b4cdec04a8b134fb7e8035eff93d
Glitch (memorygameprototype) <none>
2021-03-12 07:09:01 +0000

    🍧🐇 Updated with Glitch

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ console.log("hi");
 var pattern = [2, 2, 4, 3 ,2 ,1 ,2, 4];
 var progress = 0 ;
 var gamePlaying = false ;
+
+
+function startGame()
+{
+  //initialize the game variables
+  progress = 0; 
+  gamePlaying = true;
+
+  // swaps the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+}
+
+function stopGame()
+{
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
